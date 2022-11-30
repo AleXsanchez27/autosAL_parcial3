066e30d5b2c5835f8807c535f392c3db308778ad
AleXsanchez27 <99852421+AleXsanchez27@users.noreply.github.com>
2022-11-30 14:49:11 -0700

    Add files via upload

---
+++ AutosAL/Autos/autosAL_parcial3/nuevo_usuario.php
@@ -0,0 +1,48 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body>
+
+
+<br><br>
+<div class="card text-center" style="width: 320px; margin: auto; width: 18rem;">
+    <img src="entro/imagenes/GMC-Logo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <form action="nuevo_usuario2.php" method="POST" >
+
+            <label for="">Correo:</label>
+            <div class="form-row">
+                <input name="correo" type="text" class="form-control" placeholder="Nombre de usuario" required>  
+            </div>
+            <br>
+            <label for="">Nombre:</label>
+            <div class="form-row">
+                <input name="nombre" type="text" class="form-control" placeholder="Contraseña" required>
+            </div>
+            <br>
+            <label for="">Contraseña</label>
+            <div class="form-row">
+                <input name="contrasena" type="password" class="form-control" placeholder="Contraseña" required>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">   Crear usuario  </button>
+            <br>
+
+        </form>
+    </div> 
+</div>
+
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
